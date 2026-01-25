0b5c07318dbe567a381dd10dedba57597d3a77d4
Sosuke <kato.052wazime@gmail.com>
2026-01-25 17:45:02 +0900

    feat: return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("PIYO")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("poyo")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Printf(message)
 }
